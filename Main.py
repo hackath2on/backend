d3a49d0454b80b3d004cfec4887ec4af2ae1676c
Macbook <macbook@PC.local>
2017-05-13 20:50:20 +0200

    Firebase setup

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+API_KEY = 'FMnkufGpV3xvG9R2jQKjeVIi85nW5EIOP5sB5c2N'
+PROJECT_ID = 'hackath2on-562dd'
+BDDD_URL = 'https://hackath2on-562dd.firebaseio.com/'
+
+# Todas las requests tienen que tener este formato
+# 'https://hackath2on-562dd.firebaseio.com/entity.json?access_token=FMnkufGpV3xvG9R2jQKjeVIi85nW5EIOP5sB5c2N'
+
 
 @app.route("/")
 def hello():
