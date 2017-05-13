084e256c956f1e2b10958972d6b6d9c4b46ab2a4
Macbook <macbook@PC.local>
2017-05-13 20:55:30 +0200

    Syntax

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import requests
+
 app = Flask(__name__)
 
 API_KEY = 'FMnkufGpV3xvG9R2jQKjeVIi85nW5EIOP5sB5c2N'
 PROJECT_ID = 'hackath2on-562dd'
 BDDD_URL = 'https://hackath2on-562dd.firebaseio.com/'
+
 
 # Todas las requests tienen que tener este formato
 # 'https://hackath2on-562dd.firebaseio.com/entity.json?access_token=FMnkufGpV3xvG9R2jQKjeVIi85nW5EIOP5sB5c2N'
@@ -14,10 +16,12 @@ BDDD_URL = 'https://hackath2on-562dd.firebaseio.com/'
 def hello():
     return "Hello World!"
 
+
 @app.route("/sampleGET")
 def sample_get():
     r = requests.get(BDDD_URL + "users/SAMPLEID")
-    #tratar objecto request "r"
+    # tratar objecto request "r"
+
 
 @app.route("/users/:ID")
 def register_user():
@@ -30,6 +34,7 @@ def register_user():
 
 def main():
     app.run()
+
 
 if __name__ == '__main__':
     main()
